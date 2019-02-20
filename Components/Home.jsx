ef6c6cb33d9a7d372febcdd8a7e59d1fdb8c221e
Student <example@example.com>
2019-02-20 13:49:52 -0800

    attach new functions

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -12,32 +12,43 @@ class Home extends React.Component {
      foodLevel:10
     };
     this.handleStartGame = this.handleStartGame.bind(this);
+    this.handleFeedToma = this.handleFeedToma.bind(this);
+    this.handleSleepToma = this.handleSleepToma.bind(this);
+    this.handleBordomToma = this.handleBordomToma.bind(this);
   }
+
   handleStartGame(){
     var tempFoodLevel = this.state.foodLevel;
     var tempSleepLevel = this.state.sleepLevel;
     var tempBordomLevel = this.state.bordomLevel;
-    this.setState({tempFoodLevel: tempFoodLevel --});
-    this.setState({sleepLevel: tempSleepLevel--});
-    this.setState({bordomLevel: tempBordomLevel--});
+    this.setState({foodLevel: tempFoodLevel -1});
+    this.setState({sleepLevel: tempSleepLevel-1});
+    this.setState({bordomLevel: tempBordomLevel-1});
   }
+
 
   componentDidMount() {
  this.startTimer = setInterval(() =>
    this.handleStartGame(),
+
    3000
  );
 }
 
 
-  feedToma(){
-    this.setState({foodLevel: this.tempFoodLevel++});
+  handleFeedToma(){
+    this.setState({foodLevel: this.state.foodLevel+1});
   }
-  sleepToma(){
-    this.setState({sleepLevel: this.tempSleepLevel +=1});
+  // checkLevel(){
+  //
+  // }
+
+  handleSleepToma(){
+    this.setState({sleepLevel: this.state.sleepLevel+1});
   }
-  bordomToma(){
-    this.setState({bordomLevel: this.tempBordomLevel +=1});
+
+  handleBordomToma(){
+    this.setState({bordomLevel: this.state.bordomLevel +1});
   }
 
 
@@ -46,9 +57,14 @@ class Home extends React.Component {
     return (
       <div>
         <button onClick={this.handleStartGame}>Play game</button>
+
         <p>food level :{this.state.foodLevel}</p>
         <p>sleep level :{this.state.sleepLevel}</p>
         <p>bordomLevel :{this.state.bordomLevel}</p>
+
+        <button onClick={this.handleFeedToma}>feed</button>
+        <button onClick={this.handleSleepToma}>sleep</button>
+        <button onClick={this.handleBordomToma}>Play</button>
       </div>
 
     );
